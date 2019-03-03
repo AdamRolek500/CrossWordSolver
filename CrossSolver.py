339b0a4e3995a6f67443f3d2f16b6a30e3d4ad44
Unknown <adamrolek500@gmail.com>
2019-03-03 01:44:39 -0500

    Added Letter class and colored console text

--- CrossSolver.py
+++ CrossSolver.py
@@ -3,6 +3,9 @@
 """
 
 
+from termcolor import colored
+
+
 class Letter:
     letter = ''
     match_count = 0
@@ -21,10 +24,12 @@ def check_rows(matrix, word):
     row = ""
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
-            row += matrix[x][y]
+            row += matrix[x][y].letter
             if word in row:
                 print("Word Found!")
-                print(row)
+                start_index = row.find(word)
+                for index in range(len(word)):
+                    matrix[x][start_index + index].add_match_count()
                 return
         row = ""
 
@@ -33,23 +38,36 @@ def check_cols(matrix, word):
     col = ""
     for y in range(len(matrix)):
         for x in range(len(matrix[y])):
-            col += matrix[x][y]
+            col += matrix[x][y].letter
             if word in col:
                 print("Word Found!")
-                print(col)
+                start_index = col.find(word)
+                for index in range(len(word)):
+                    matrix[start_index + index][y].add_match_count()
                 return
         col = ""
 
 
+def print_matrix(matrix):
+        for x in range(len(matrix)):
+            for y in range(len(matrix[x])):
+                if matrix[x][y].get_match_count() != 0:
+                    print(colored(matrix[x][y].letter, "red"), end="")
+                else:
+                    print(matrix[x][y].letter, end="")
+            print()
+
+
 def main():
-    word = "abg"
-    matrix = [['a', 'a', 'a', 'a', 'a'],
-              ['b', 'b', 'b', 'b', 'b'],
-              ['c', 'c', 'g', 'c', 'c'],
-              ['d', 'd', 'd', 'd', 'd'],
-              ['e', 'e', 'e', 'e', 'e']]
+    word = "cg"
+    matrix = [[Letter('a'), Letter('a'), Letter('a'), Letter('a'), Letter('a')],
+              [Letter('b'), Letter('b'), Letter('b'), Letter('b'), Letter('b')],
+              [Letter('c'), Letter('c'), Letter('c'), Letter('g'), Letter('c')],
+              [Letter('d'), Letter('d'), Letter('d'), Letter('d'), Letter('d')],
+              [Letter('e'), Letter('e'), Letter('e'), Letter('e'), Letter('e')]]
     check_rows(matrix, word)
     check_cols(matrix, word)
+    print_matrix(matrix)
 
 
 if __name__ == "__main__":
